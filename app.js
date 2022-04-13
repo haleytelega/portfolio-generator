e65c8abba5d798e9c776d1b1557efac890ee1273
Haley Telega <haleytelega@outlook.com>
2022-04-13 12:38:45 -0400

    added first question

--- app.js
+++ app.js
@@ -12,20 +12,26 @@
 
 // printProfileData(profileDataArgs);
 
+const inquirer = require('inquirer');
 
-const fs = require('fs');
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'what is your name?'
+    }
+])
+.then(answers => console.log(answers));
+// const fs = require('fs');
 
-const generatePage = require('./src/page-template.js'); //put at the top in files we want to recieve those exported functions
+// const generatePage = require('./src/page-template.js'); //put at the top in files we want to recieve those exported functions
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 
+// fs.writeFile('./index.html', pageHTML, err =>{ //first is the filename, then the data being written (so the html string) and then handles errors
+//     if (err) throw err;
 
-
-fs.writeFile('index.html', generatePage(name, github), err =>{ //first is the filename, then the data being written (so the html string) and then handles errors
-    if (err) throw err;
-
-    console.log("portfolio complete! check out index.html to see the output!");
-});
+//     console.log("portfolio complete! check out index.html to see the output!");
+// });
